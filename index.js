81d8e100e995cb5b4f68979bb5834cd78444fc5c
Michael Dudek <michael.dudek@centrum.cz>
2018-04-27 12:33:08 +0200

    - updated code to be compatible with webpack versions < 4

--- index.js
+++ index.js
@@ -7,17 +7,32 @@ class CompileTimePlugin {
   }
 
   apply(compiler) {
-    compiler.hooks.compilation.tap('CompileTimePlugin', () => {
-      this.startTime = moment();
-    });
-
-    compiler.hooks.emit.tapAsync('CompileTimePlugin', (compilation, callback) => {
-      const end = moment().diff(this.startTime);
-
-      console.log(`\n\nCompile done in: ${end/1000}s`.yellow.bold);
-
-      callback();
-    });
+    if (compiler.hooks) {
+      compiler.hooks.compilation.tap('CompileTimePlugin', () => {
+        this.startTime = moment();
+      });
+  
+      compiler.hooks.emit.tapAsync('CompileTimePlugin', (compilation, callback) => {
+        const end = moment().diff(this.startTime);
+  
+        console.log(`\n\nCompile done in: ${end/1000}s`.yellow.bold);
+  
+        callback();
+      });
+    }
+    else {
+      compiler.plugin("compilation", () => {
+        this.startTime = moment();
+      });
+    
+      compiler.plugin("emit", (compilation, callback) => {
+        const end = moment().diff(this.startTime);
+    
+        console.log(`\n\nCompile done in: ${end/1000}s`.yellow.bold);
+    
+        callback();
+      });
+    }
   }
 }
 
