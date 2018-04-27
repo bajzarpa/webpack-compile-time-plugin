7ac27c7708aca5ba9cd4a9cf796d4f1627a9061b
Michael Dudek <michael.dudek@centrum.cz>
2018-04-27 12:19:56 +0200

    - updated code according to Webpack v4 changes

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const colors = require('colors');
 const moment = require('moment');
 
-function CompileTimePlugin(){
-  this.startTime = null;
+class CompileTimePlugin {
+  constructor() {
+    this.startTime = null;
+  }
+
+  apply(compiler) {
+    compiler.hooks.compilation.tap('CompileTimePlugin', () => {
+      this.startTime = moment();
+    });
+
+    compiler.hooks.emit.tapAsync('CompileTimePlugin', (compilation, callback) => {
+      const end = moment().diff(this.startTime);
+
+      console.log(`\n\nCompile done in: ${end/1000}s`.yellow.bold);
+
+      callback();
+    });
+  }
 }
-
-
-CompileTimePlugin.prototype.apply = function(compiler) {
-  compiler.plugin("compilation", () => {
-    this.startTime = moment();
-  });
-
-  compiler.plugin("emit", (compilation, callback) => {
-    const end = moment().diff(this.startTime);
-
-    console.log(`\n\nCompile done in: ${end/1000}s`.yellow.bold);
-
-    callback();
-  });
-
-};
 
 module.exports = CompileTimePlugin;
